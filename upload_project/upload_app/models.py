60b3ca12d77dcf8ccfe192a8eaddc4b2bd4c2253
Aroduno <antevasin.ryosuke@gmail.com>
2023-06-05 17:23:19 +0900

    Jun5th

--- upload_project/upload_app/models.py
+++ upload_project/upload_app/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 class UploadImage(models.Model):
     image = models.ImageField(upload_to='img/')
+    path = models.CharField(max_length=200, default='')
+    def __str__(self):
+        return self.path
+
+class Image(models.Model):
+    path = models.CharField(max_length=200)
+    def __str__(self):
+        return self.path
--- upload_project/upload_app/views.py
+++ upload_project/upload_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import UploadForm
 from .models import UploadImage
+from .models import Image
 
 def index(request):
     params = {
@@ -16,3 +17,24 @@ def index(request):
             params['id'] = upload_image.id
 
     return render(request, 'upload_app/index.html', params)
+
+# def home(request):
+#     return render(request, 'upload_app/home.html', {})
+
+# def image_list(request):
+from PIL import Image
+
+def home(request):
+    images = UploadImage.objects.all()
+    # for image in images:
+    #     img = Image.open(image.image.url)
+    #     width, height = img.size
+    #     aspect_ratio = height / width
+    #     new_width = 400  # The width that you want
+    #     new_height = int(new_width * aspect_ratio)
+    #     img = img.resize((new_width, new_height))
+    #     img.save(image.image.url)
+    return render(request, 'upload_app/home.html', {'images': images})
+
+def link(request):
+   return render(request, 'upload_app/link.html', {}) 
